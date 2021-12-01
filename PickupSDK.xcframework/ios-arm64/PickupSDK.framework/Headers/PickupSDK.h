ef394019240461f20bfb4b378d5066635f5347f7
Gaurav Shishodia <>
2021-12-01 16:49:04 +0530

    Added Framework

---
+++ PickupSDK.xcframework/ios-arm64/PickupSDK.framework/Headers/PickupSDK.h
@@ -0,0 +1,18 @@
+//
+//  PickupSDK.h
+//  PickupSDK
+//
+//  Created by Gaurav Shishodia on 04/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PickupSDK.
+FOUNDATION_EXPORT double PickupSDKVersionNumber;
+
+//! Project version string for PickupSDK.
+FOUNDATION_EXPORT const unsigned char PickupSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PickupSDK/PublicHeader.h>
+
+
