d308299fdc2109511b524654a917dc1ec0f3ebb6
Gaurav Shishodia <>
2021-12-12 22:17:58 +0530

    updated sdk

--- PickupSDK.xcframework/ios-arm64/PickupSDK.framework/Headers/PickupSDK-Swift.h
+++ PickupSDK.xcframework/ios-arm64/PickupSDK.framework/Headers/PickupSDK-Swift.h
@@ -332,6 +332,7 @@ SWIFT_CLASS("_TtC9PickupSDK18PickupMetaResModel")
 
 SWIFT_CLASS("_TtC9PickupSDK26PickupNotificationResModel")
 @interface PickupNotificationResModel : NSObject
+@property (nonatomic, copy) NSString * _Nullable message;
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 @end
